d27c1bbe6a49d8abc6c2a7ad224bb61a6b4771ec
mughni02 <faizalmughni@student.uir.ac.id>
2025-06-28 22:25:03 +0700

    Deskripsi update

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <h1><i class="fas fa-terminal"></i> Jalankan Perintah</h1>
+    <h1><i class="fas fa-terminal"></i>Faiz Almughni_233510304</h1>
   </div>
 </template>
 
